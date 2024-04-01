632638465225ef919b4a11e90cfe60c2169ed2e0
avanish7390 <singhavanish7700@gmail.com>
2024-04-01 22:49:34 +0530

    navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello!
-    </div>
+     <Navbar></Navbar>
   )
 }
 
