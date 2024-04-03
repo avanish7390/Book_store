716cc605d80facbb8d148c21c579f3d1735fa89b
avanish7390 <singhavanish7700@gmail.com>
2024-04-04 00:07:40 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Freebook from './components/Freebook'
+import Home from './home/Home'
+import { Route, Routes } from  "react-router-dom"
+import Courses from './courses/Courses'
+import Signup from './components/Signup'
+
+
 
 
 const App = () => {
   return (
      <>
-     <Navbar/>
-     <Banner/>
-     <Freebook/>
-     <Footer />
+     <div className='dark:bg-slate-900 dark:text-white'>
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/course' element={<Courses/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+     </Routes>
+     </div>
      </>
   )
 }
 
-export default App;
+export default App
