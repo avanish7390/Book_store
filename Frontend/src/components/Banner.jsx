0056326b4363be750364b6818f147e7135bcdb46
avanish7390 <singhavanish7700@gmail.com>
2024-04-06 23:01:06 +0530

    update

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -17,7 +17,7 @@ function Banner() {
         <input type="text" className="grow" placeholder="Email" />
         </label>
         </div>
-        <button className="btn mt-6 btn-active btn-secondary">Secondary</button>
+        <button className="btn mt-6 btn-active btn-secondary">Get Started</button>
        </div>
        <div className=' order-1 w-full md:w-1/2'><img src={banner} className=' w-92 h-92' alt=''/></div>
       
