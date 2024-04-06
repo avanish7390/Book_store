165f0c8fdcc548e63483237dd931e70561d66ba2
avanish7390 <singhavanish7700@gmail.com>
2024-04-06 22:58:44 +0530

    update

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -5,12 +5,16 @@ const About = () => {
     <div className="  dark:bg-slate-800 dark:text-white" >
       <div className="md:w-1/2 mx-auto">
         <h1 className="text-2xl text-center mt-20">About Us</h1>
-        <p className="text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor
-          turpis vel commodo scelerisque. Phasellus consequat, ante quis
-          pharetra rhoncus, mi lacus venenatis nisl, vel aliquet lorem nisi et
-          sapien.
-        </p>
+        <div className="hero min-h-screen ">
+  <div className="hero-content  lg:flex-row">
+    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-6xl font-bold">Box Office News!</h1>
+      <p className="py-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
       </div>
     </div>
   );
